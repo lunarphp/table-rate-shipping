e3755a0e0d7f57fb04ac5828db39a102d90d6458
Michal Urva <michalurva@gmail.com>
2025-04-11 19:18:34 +0200

    WIP

--- src/Filament/Resources/ShippingMethodResource/Pages/ManageShippingMethodAvailability.php
+++ src/Filament/Resources/ShippingMethodResource/Pages/ManageShippingMethodAvailability.php
@@ -13,8 +13,7 @@ class ManageShippingMethodAvailability extends BaseManageRelatedRecords
 {
     protected static string $resource = ShippingMethodResource::class;
 
-    // We'll use a different relationship approach since we need multiple relationships
-    protected static ?string $relationship = '';
+    protected static ?string $relationship = 'channels';
 
     public function getTitle(): string
     {
