0abbc7d768e41ee68e5e9b6f9ac456fd07a205e6
Michal Urva <michalurva@gmail.com>
2025-04-11 17:59:22 +0200

    Implement migration for channel_shipping_method table with foreign key constraints and default values

---
+++ database/migrations/2024_07_29_100000_create_channel_shipping_method_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Lunar\Base\Migration;
+use Lunar\Models\Channel;
+use Lunar\Shipping\Models\ShippingMethod;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create($this->prefix . 'channel_shipping_method', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Channel::class)->constrained(
+                $this->prefix . 'channels'
+            );
+            $table->foreignIdFor(ShippingMethod::class)->constrained(
+                $this->prefix . 'shipping_methods'
+            );
+            $table->boolean('visible')->default(true)->index();
+            $table->boolean('enabled')->default(true)->index();
+            $table->timestamp('starts_at')->nullable()->index();
+            $table->timestamp('ends_at')->nullable()->index();
+            $table->timestamps();
+
+            $table->unique(['channel_id', 'shipping_method_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists($this->prefix . 'channel_shipping_method');
+    }
+};
